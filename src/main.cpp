65230441946ce131f37f83632925413b61521fb6
himax16 <himawan.winarto@yahoo.co.id>
2022-04-21 18:51:41 -0400

    Enable version report

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,22 @@
 // C headers
 
 // C++ headers
+#include <iostream>
 
 // External libraries headers
 
 // Project headers
-#include "config.hpp"
+#include "config.h"
 
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    // Report version
+    std::cout << "Project " << PROJECT_NAME
+              << " Version " << PROJECT_VER << std::endl;
+    std::cout << "Git hash: " << PROJECT_GITHASH
+              << " (" << PROJECT_GITSTATUS << ")" << std::endl;
+    return 1;
+  }
 
   return(0);
 }
