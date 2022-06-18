8e3195c7e0740f82fc57a5bfa2a7fbe4567002e9
Himawan Winarto <himawan.winarto@yahoo.co.id>
2022-06-18 22:55:57 +0200

    Add some hypothetical initialization

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ F**L**uid-K**I**netic P**L**asma S**I**mulator
 * Modern and optimized input/output
 * Non-blocking calculations
 * Data layout that can be easily transformed into a GPU code
+
+## Error codes
+### Initialization
+* `10`: Number of simulation system is not a positive integer
+* `11`: Unrecognized simulation system
+* `12`: Multiple MHD system
+
+
+## Idea notes
+1. Use array of function pointer for k-stage evolutions for all system variable
+  * Make sure at the end of every stage there is MPI wait or something similar.
+  * At each, stage the integrator can have multiple substage depending on order of accuracy.
+  * Easy to change with function pointers. Need to probably define a variable.
+  * Have composite function for example in the case of expanding box transition.
+  * This way field can also just be mesh variable.
+  * Issue might arise on how to keep the variable cache in place.
+    * Try to rearrange process call, i.e. `(v1_1, v2_1, v3_1, v3_2, v2_2[null], v1_2[null], v3_3, v1_3, v2_3, v2_4, ...)`
+    * Default option might just be cyclic with `(v1_1, ..., vn_1, vn_2, ..., v1_2, v1_3, ..., vn_3, vn_4, ...)`
+2. 
--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,14 @@
 // -----------------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
+  int n_simsys = 0;
+  bool has_mhd = false;
+  bool use_emf = true;
+
+  int i_loop = 0;
+  int n_loop = 10;
+
+  // parse_arguments(argc, argv);
   if (argc < 2) {
     // Report version
     std::cout << "Project " << PROJECT_NAME
@@ -35,15 +43,52 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  boost::json::object obj;
-  obj["pi"] = boost::math::constants::pi<double>();
+  // parse_input(input_file);
 
-  std::cout << obj << std::endl;
+  // Initialize simulation system based on the input file
+  if (n_simsys <= 0) {
+    std::cerr << "Number of simulated system must be positive" << std::endl;
+    exit(10);
+  }
+  for (int i_simsys = 0; i_simsys < n_simsys; ++i_simsys) {
+    switch (simsys[i_simsys].type) {
+      case 'm':
+        if (has_mhd) {
+          std::cerr << "There are multiple MHD simulation system" << std::endl;
+          exit(12);
+        }
+        initialize_mhdsys(simsys[i_simsys].property);
+        has_mhd = true;
+      case 'p':
+        initialize_parsys(simsys[i_simsys].property);
+        break;
+      case 'f':
+        initialize_flusys(simsys[i_simsys].property);
+        break;
+      case 'n':
+        initialize_neusys(simsys[i_simsys].property);
+        break;
+      default:
+        std::cerr << "Unrecognized simulation system" << std::endl;
+        exit(11);
+    }
+  }
 
-  int i_loop = 0;
-  int n_loop = 10;
+  // Initialize field variable if needed
+  if (use_emf) {
+    initialize_emfsys();
+  }
+
+
+  // boost::json::object obj;
+  // obj["pi"] = boost::math::constants::pi<double>();
+
+  // std::cout << obj << std::endl;
+
+  // LILI main loop
   for (i_loop = 0; i_loop < n_loop; ++i_loop) {
     std::cout << i_loop << std::endl;
+
   }
 
   return(0);
