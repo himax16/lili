caaab2d54905bd9ea677fb5d474c090343902fe2
Himawan Winarto <himawan@winarto.net>
2023-10-06 05:35:17 -0400

    Add particle initialization

---
+++ src/particle_initialization/particle_initialization.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file particle_initialization.hpp
+ * @brief Header file for particle initialization routines
+ */
+#pragma once
+
+#include <vector>
+
+#include "particle.hpp"
+
+#ifndef __LILIP_DEFAULT_GTSIZE
+#define __LILIP_DEFAULT_GTSIZE 10000
+#endif
+
+#ifndef __LILIP_DEFAULT_GTMULT
+#define __LILIP_DEFAULT_GTMULT 20
+#endif
+
+namespace lili::particle {
+void DistributeLocationUniform(Particles& particles, const int seed,
+                               const double x0, const double x1,
+                               const double y0, const double y1,
+                               const double z0, const double z1);
+
+// Gamma table
+class GammaTable {
+ public:
+  GammaTable();
+  GammaTable(std::vector<double> cdf, std::vector<double> gamma);
+
+  // Getters
+  bool interpolate() const { return interpolate_; }
+  uint32_t capacity() const { return capacity_; }
+  double cdf(const uint32_t i) const { return cdf_[i]; }
+  double gamma(const uint32_t i) const { return gamma_[i]; }
+
+  // Setters
+  bool& interpolate() { return interpolate_; }
+
+  // Function to get gamma from CDF
+  double GetGamma(const double cdf) const;
+
+ private:
+  bool interpolate_;
+  uint32_t capacity_;
+  std::vector<double> cdf_;
+  std::vector<double> gamma_;
+};
+
+// Gamma table initialization
+GammaTable GTMonoenergetic(const double gamma);
+GammaTable GTUniform(const double gamma_min, const double gamma_max);
+GammaTable GTMaxwellian3D(const double theta);
+
+// Velocity initialization
+void AddBulkVelocity(Particles& particles, const double u, const double v,
+                     const double w);
+void DistributeVelocityUniform(Particles& particles, const int seed,
+                               GammaTable& energy_table);
+
+}  // namespace lili::particle
