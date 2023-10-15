3ddbb017713bafdb6479cb472f413901c1c3649e
Himawan Winarto <himawan@winarto.net>
2023-10-15 13:57:27 -0400

    Add MoveBoris2D

--- src/particle_mover/particle_mover.cpp
+++ src/particle_mover/particle_mover.cpp
@@ -4,6 +4,8 @@
  */
 #include "particle_mover.hpp"
 
+#include <cmath>
+
 namespace lili::particle {
 /**
  * @brief Initialize ParticleMover
@@ -35,9 +37,9 @@ void ParticleMover::InitializeMover(const input::Input&) {
  * Field object
  */
 void ParticleMover::MoveBoris2D(Particles& particles, mesh::Field& field) {
-  std::cout << "Moving particles using Boris particle mover" << std::endl;
   // Initialize variables
   int npar = particles.npar();
+  double qmhdt = particles.q() / (2.0 * particles.m() * dt_);
 
   // Get the particle information
   double* __restrict__ x = particles.x();
@@ -48,36 +50,52 @@ void ParticleMover::MoveBoris2D(Particles& particles, mesh::Field& field) {
   double* __restrict__ v = particles.v();
   double* __restrict__ w = particles.w();
 
-  // Debug
-  std::cout << "x = " << x[0] << std::endl;
-  std::cout << "y = " << y[0] << std::endl;
-  std::cout << "z = " << z[0] << std::endl;
-
-  std::cout << "u = " << u[0] << std::endl;
-  std::cout << "v = " << v[0] << std::endl;
-  std::cout << "w = " << w[0] << std::endl;
-
-  // Get the Field information
-  double* __restrict__ ex = field.ex.data();
-
-  double sum = 0.;
+  double ex, ey, ez, bx, by, bz;
+  double um, vm, wm, up, vp, wp;
+  double temp;
 
   // Loop over the particles
   for (int i = 0; i < npar; ++i) {
-    sum += ex[i];
-    // Get the particle position and velocity
-    // double x = particles.x(i);
-    // double y = particles.y(i);
-    // double z = particles.z(i);
+    // Get the electromagnetic field
+    ex = qmhdt * field.ex(x[i], y[i], z[i]);
+    ey = qmhdt * field.ey(x[i], y[i], z[i]);
+    ez = qmhdt * field.ez(x[i], y[i], z[i]);
 
-    // double u = particles.u(i);
-    // double v = particles.v(i);
-    // double w = particles.w(i);
+    bx = qmhdt * field.bx(x[i], y[i], z[i]);
+    by = qmhdt * field.by(x[i], y[i], z[i]);
+    bz = qmhdt * field.bz(x[i], y[i], z[i]);
 
-    // // Get the electric field
-    // double ex = field.ex(x, y, z);
-    // double ey = field.ey(x, y, z);
-    // double ez = field.ez(x, y, z);
+    // First half acceleration
+    um = u[i] + ex;
+    vm = v[i] + ey;
+    wm = w[i] + ez;
+
+    // First half of the rotation
+    temp = 1.0 / std::sqrt(1.0 + um * um + vm * vm + wm * wm);
+    bx *= temp;
+    by *= temp;
+    bz *= temp;
+
+    temp = 2.0 / (1.0 + bx * bx + by * by + bz * bz);
+    up = (um + vm * bz - wm * by) * temp;
+    vp = (vm + wm * bx - um * bz) * temp;
+    wp = (wm + um * by - vm * bx) * temp;
+
+    // Second half acceleration
+    um = um + ex + vp * bz - wp * by;
+    vm = vm + ey + wp * bx - up * bz;
+    wm = wm + ez + up * by - vp * bx;
+
+    // Advance position
+    temp = 1.0 / std::sqrt(1.0 + um * um + vm * vm + wm * wm);
+    x[i] += dt_ * um * temp;
+    y[i] += dt_ * vm * temp;
+    z[i] += dt_ * wm * temp;
+
+    // Update velocity
+    u[i] = um;
+    v[i] = vm;
+    w[i] = wm;
   }
 }
 }  // namespace lili::particle
