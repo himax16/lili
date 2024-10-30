1ecc9865022ec1f1110a5a5ca1575e0c2328b4a3
Himawan Winarto <himawan@winarto.net>
2024-10-30 15:56:57 -0400

    Add script to upload documentation

---
+++ upload_docs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Variables
+GITHUB_USERNAME="himax16"
+REPO_NAME="lili-docs"
+
+BUILD_DOCS_DIRECTORY="build/docs/sphinx"
+DOXYGEN_DIRECTORY="build/docs/doxygen/html"
+cd $BUILD_DOCS_DIRECTORY
+
+# Remove any existing git information
+rm -rf .git
+
+# Add .nojekyll file to the directory
+touch .nojekyll
+
+# Copy doxygen files to the root directory
+cp -r $DOXYGEN_DIRECTORY ./doxygen
+
+# Initialize a new git repository
+git init
+git remote add origin git@github.com:$GITHUB_USERNAME/$REPO_NAME.git
+
+# Add and commit files
+git add .
+git commit -m "Push documentation"
+
+# Force push to GitHub
+git push -u origin master --force
